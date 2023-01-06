92f1a8756db9862ebb781f3ad3ffff43138e2273
onpyeong <gsh447114@gmail.com>
2023-01-07 03:05:57 +0900

    [FEAT] 관측 날씨 조회 controller 구현

--- src/controllers/ScheduleController.ts
+++ src/controllers/ScheduleController.ts
@@ -1,3 +1,19 @@
+import { Request, Response } from "express";
+import ScheduleService from "../services/ScheduleService";
+import sc from "../modules/statusCode";
+import rm from "../modules/responseMessage";
+import util from "../modules/util"
+
+//* 관측 날씨 조회
+const getObserved = async (req: Request, res: Response) => {
+
+  const data = await ScheduleService.getObserved();
+
+  if (!data) return res.status(sc.INTERNAL_SERVER_ERROR).send(util.fail(sc.INTERNAL_SERVER_ERROR, rm.READ_OBSERVED_WEATHER_FAIL));
+
+  return res.status(sc.OK).send(util.success(sc.OK, rm.READ_OBSERVED_WEATHER_SUCCESS, data));
+};
+
 export default {
-    
-  };
+  getObserved,
+};
--- src/routes/ScheduleRouter.ts
+++ src/routes/ScheduleRouter.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+import { ScheduleController } from '../controllers';
 
 const router = Router();
 
+router.get('/observed', ScheduleController.getObserved);
 export default router;
