947310ec70b58dafa6ce222ff2fb848d94987e8f
kimminwook <coreminw@naver.com>
2023-01-08 02:30:25 +0900

    [CHORE] Schedule Hourly 개발구현중

--- src/routes/ScheduleRouter.ts
+++ src/routes/ScheduleRouter.ts
@@ -1,9 +1,10 @@
-import { Router } from 'express';
-import ScheduleController from '../controllers/ScheduleController';
+import { Router } from "express";
+import ScheduleController from "../controllers/ScheduleController";
 
 const router = Router();
 
-router.post('/observed', ScheduleController.createObserved);
-router.post('/forecast/daily', ScheduleController.createDailyForecast);
+router.post("/observed", ScheduleController.createObserved);
+router.post("/forecast/daily", ScheduleController.createDailyForecast);
+router.post("/forecast/hourly", ScheduleController.createHourlyForecast);
 
 export default router;
